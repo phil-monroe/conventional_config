9c756ffe7f56594f8d66d959c66d427d90bdbe03
Tejas R. Mandke <tejas@identified.com>
2013-10-16 10:16:08 -0700

    make it possible to use the gem without rails

--- conventional_config.gemspec
+++ conventional_config.gemspec
@@ -16,7 +16,6 @@ Gem::Specification.new do |s|
   s.files = Dir["{app,config,db,lib}/**/*"] + ["MIT-LICENSE", "Rakefile", "README.rdoc"]
   s.test_files = Dir["test/**/*"]
 
-  s.add_dependency "rails", ">= 3.2.9"
   s.add_dependency "hashie"
   # s.add_dependency "jquery-rails"
 
--- lib/conventional_config/engine.rb
+++ lib/conventional_config/engine.rb
@@ -1,10 +1,11 @@
-module ConventionalConfig
-  class Engine < ::Rails::Engine
+if defined?(::Rails)
+  module ConventionalConfig
+    class Engine < ::Rails::Engine
 
-    initializer 'conventional_config.load_yaml_configs', before: :load_environment_hook do |app|
-      paths = app.config.paths['config/yamls'] || ["#{app.root}/config/settings"]
-      ConventionalConfig.generate_classes! paths
+      initializer 'conventional_config.load_yaml_configs', before: :load_environment_hook do |app|
+        paths = app.config.paths['config/yamls'] || ["#{app.root}/config/settings"]
+        ConventionalConfig.generate_classes! paths
+      end
     end
-
   end
 end
